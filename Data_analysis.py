8a8b1cb193a39b2db69b28f6186b3e575f1b2219
RebeccaR1988 <rebecca.ruthe@t-online.de>
2026-09-18 15:05:57 +0200

    Integrate summary statistics and visualization functions
    Added functions to calculate statistics and visualize data.

--- Data_analysis.py
+++ Data_analysis.py
@@ -3,6 +3,9 @@ import pandas as pd
 from Iris_data_main import load_dataset as datacleaning
 from Iris_data_main import check_dimensions as variablescheck
 from Iris_data_main import inspect_missing_values as checknulls
+from statistics import calculate_summary_statistics as stats
+from visualization import plot_histograms as histogram
+from visualization import plot_boxplots as boxplots
 
 # Download raw CSV directly from GitHub
 url = "https://raw.githubusercontent.com/mwaskom/seaborn-data/master/iris.csv"
@@ -15,3 +18,6 @@ print("Saved iris.xlsx successfully!")
 datacleaning()
 variablescheck()
 checknulls()
+stats()
+histogram()
+boxplots()
