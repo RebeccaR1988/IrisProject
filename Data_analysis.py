d9b4a7ae6b3aff6b690e44e78b412f68ecb12555
RebeccaR1988 <rebecca.ruthe@t-online.de>
2026-09-18 14:49:52 +0200

    Import datacleaning module in Data_analysis.py
    Imported datacleaning module for data processing.

--- Data_analysis.py
+++ Data_analysis.py
@@ -1,5 +1,7 @@
 import pandas as pd
 
+import Iris_data_main.py as datacleaning
+
 # Download raw CSV directly from GitHub
 url = "https://raw.githubusercontent.com/mwaskom/seaborn-data/master/iris.csv"
 df = pd.read_csv(url)
@@ -7,3 +9,5 @@ df = pd.read_csv(url)
 # Save directly to Excel (.xlsx)
 df.to_excel("iris.xlsx", index=False)
 print("Saved iris.xlsx successfully!")
+
+datacleaning
